18179b973931776d202c1b4b212096a96e93f502
Pakin Kraisin <dxpakin@gmail.com>
2023-01-09 16:41:48 +0700

    init course

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -2,9 +2,11 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PrismaModule } from './prisma/prisma.module';
+import { CourseModule } from './course/course.module';
+import { CourseModule } from './course/course.module';
 
 @Module({
-  imports: [PrismaModule],
+  imports: [PrismaModule, CourseModule],
   controllers: [AppController],
   providers: [AppService],
 })
