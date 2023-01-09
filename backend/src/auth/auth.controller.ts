9a6450282243eeb4681800ad675f04070434465e
Tee <teetat.wis@gmail.com>
2023-01-10 00:22:55 +0700

    test api

---
+++ backend/src/auth/auth.controller.ts
@@ -0,0 +1,19 @@
+import { Controller, Post, Req } from '@nestjs/common';
+import { AuthService } from './auth.service';
+import { Request } from 'express';
+
+@Controller('auth')
+export class AuthController {
+  constructor(private readonly authService: AuthService) {}
+
+  @Post('signup')
+  signup(@Req() req: Request) {
+    console.log(req);
+    return this.authService.signup();
+  }
+
+  @Post('signin')
+  signin() {
+    return this.authService.signin();
+  }
+}
