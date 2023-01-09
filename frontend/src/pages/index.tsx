da72472275ed105cac05892afd66fc002367edc7
MaStEr <masternonnolnw@gmail.com>
2023-01-09 15:36:04 +0700

    move test Text to other file

--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -8,26 +8,6 @@ export default function home() {
     <>
       <Text size="lg">test</Text>
       <LandingPage></LandingPage>
-      <Title order={1}>Learning and teaching online, made easy.</Title>
-      <Title order={2}>Learning and teaching online, made easy.</Title>
-      <Title order={3}>Learning and teaching online, made easy.</Title>
-      <Title order={4}>Learning and teaching online, made easy.</Title>
-      <BodyText size="xl">
-        Practice your English and learn new things with the platform. Make
-        learning words more fun with mini-games
-      </BodyText>
-      <BodyText size="lg">
-        Practice your English and learn new things with the platform. Make
-        learning words more fun with mini-games
-      </BodyText>
-      <BodyText size="md">
-        Practice your English and learn new things with the platform. Make
-        learning words more fun with mini-games
-      </BodyText>
-      <BodyText size="sm">
-        Practice your English and learn new things with the platform. Make
-        learning words more fun with mini-games
-      </BodyText>
     </>
   );
 }
