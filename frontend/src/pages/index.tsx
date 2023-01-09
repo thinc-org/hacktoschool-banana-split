26796b39c3d3eda7e97f595c90ecbac6a0ee2aca
Thita Soodphakdee <thita@kkumail.com>
2023-01-09 15:17:06 +0700

    edit landingPage

--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -1,10 +1,12 @@
 import { Text } from "@mantine/core";
+import LandingPage from "../../module/LandingPage";
 
 export default function home() {
   // return <div style={{ fontFamily: "Montserrat" }}>Banana Split Team</div>;
   return (
     <>
       <Text size="lg">test</Text>
+      <LandingPage></LandingPage>
     </>
   );
 }
