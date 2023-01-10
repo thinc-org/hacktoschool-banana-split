1870afa7050c929e110cb6af6b32bed2affab43b
Tee <teetat.wis@gmail.com>
2023-01-10 16:48:00 +0700

    create signup api

--- backend/src/auth/auth.service.ts
+++ backend/src/auth/auth.service.ts
@@ -1,15 +1,34 @@
-import { Injectable, Post } from '@nestjs/common';
+import { ForbiddenException, Injectable, Post } from '@nestjs/common';
 import { PrismaService } from 'src/prisma/prisma.service';
 import * as argon from 'argon2';
 import { AuthDto } from './dto';
+import { PrismaClient } from '@prisma/client';
+import { PrismaClientKnownRequestError } from '@prisma/client/runtime';
 
 @Injectable()
 export class AuthService {
   constructor(private prisma: PrismaService) {}
   async signup(dto: AuthDto) {
     const hashpwd = await argon.hash(dto.password);
-    console.log(hashpwd);
-    return { msg: 'I have signed up' };
+
+    try {
+      const user = await this.prisma.user.create({
+        data: {
+          email: dto.email,
+          password: hashpwd,
+          firstName: dto.firstname,
+          lastName: dto.lastname,
+        },
+      });
+      return user;
+    } catch (err) {
+      if (err instanceof PrismaClientKnownRequestError) {
+        if (err.code === 'P2002') {
+          throw new ForbiddenException('Email Taken');
+        }
+      }
+      throw err;
+    }
   }
 
   signin() {
