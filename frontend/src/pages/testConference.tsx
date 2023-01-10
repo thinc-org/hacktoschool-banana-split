e3efbc1f824fc0c3f7fd02f585c37cf0859cfa05
MaStEr <masternonnolnw@gmail.com>
2023-01-10 12:40:13 +0700

    init conference server and add testConference

---
+++ frontend/src/pages/testConference.tsx
@@ -0,0 +1,62 @@
+import { io } from "socket.io-client";
+import { memo, useEffect, useRef, useState } from "react";
+
+function addVideoStream(video: any, stream: any) {
+  video.current.srcObject = stream;
+  video.current.addEventListener("loadedmetadata", () => {});
+}
+function connectToNewUser(userId: any, stream: any, peer: any, video: any) {
+  const call = peer.call(userId, stream);
+  // const video = document.createElement("video");
+  call.on("stream", (userVideoStream: any) => {
+    addVideoStream(video, userVideoStream);
+  });
+  call.on("close", () => {
+    video.remove();
+  });
+}
+function testConference() {
+  const [userId, setUserId] = useState("");
+
+  const videos = useRef<HTMLVideoElement>(null);
+  const videos2 = useRef<HTMLVideoElement>(null);
+  const videosContainer = useRef<HTMLDivElement>(null);
+  useEffect(() => {
+    if (userId == "") {
+      import("peerjs").then(({ default: Peer }) => {
+        const peer = new Peer();
+        peer.on("open", (id) => {
+          socket.emit("join-room", roomId, id);
+          setUserId(id);
+        });
+
+        const roomId = "c4f0ecbf-6ae5-4edd-b84d-d16f1d821496";
+        const socket = io("http://localhost:8000/");
+        navigator.mediaDevices
+          .getUserMedia({ video: true, audio: false })
+          .then((stream) => {
+            peer.on("call", (call) => {
+              call.answer(stream);
+            });
+            socket.on("user-connected", (userId) => {
+              console.log("new user connected: " + userId);
+              connectToNewUser(userId, stream, peer, videos2);
+            });
+            socket.on("user-disconnected", (userId) => {
+              console.log("User disconnected: " + userId);
+            });
+            addVideoStream(videos, stream);
+          });
+      });
+    }
+  }, []);
+  return (
+    <>
+      <div ref={videosContainer}>
+        <video ref={videos} autoPlay></video>
+        <video ref={videos2} autoPlay></video>
+      </div>
+    </>
+  );
+}
+export default memo(testConference);
