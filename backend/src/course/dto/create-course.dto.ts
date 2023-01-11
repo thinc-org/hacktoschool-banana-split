d7d10d0c24fa124e00a36dbf0728ec285e7394e2
Pakin Kraisin <dxpakin@gmail.com>
2023-01-11 21:18:49 +0700

    refactor(course): remove unnecessary stuff

--- backend/src/course/dto/create-course.dto.ts
+++ backend/src/course/dto/create-course.dto.ts
@@ -2,13 +2,10 @@ import { IsNotEmpty, isNumber, IsNumber, IsString } from 'class-validator';
 
 export class CreateCourseDto {
   @IsString()
-  @IsNotEmpty()
   title: string;
   @IsString()
-  @IsNotEmpty()
   description: string;
 
   @IsNumber()
-  @IsNotEmpty()
   instructorId: number;
 }
--- backend/src/course/dto/update-course.dto.ts
+++ backend/src/course/dto/update-course.dto.ts
@@ -1,7 +1,7 @@
 import { MappedType, PartialType } from '@nestjs/mapped-types';
 import { CreateCourseDto } from './create-course.dto';
 
-export class UpdateCourseDto extends PartialType(CreateCourseDto) {
+export class UpdateCourseDto {
   data: MappedType<Partial<CreateCourseDto>>;
   userIdsToAdd?: number[];
 }
