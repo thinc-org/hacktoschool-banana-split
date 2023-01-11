c7e43cd9939eeea78cf96aae2c2919cba665b090
Pakin Kraisin <dxpakin@gmail.com>
2023-01-11 22:03:46 +0700

    feat(user): user also some features
    also includes /user/instructor-courses/id
    and
    /user/student-courses/id

--- backend/src/user/dto/create-user.dto.ts
+++ backend/src/user/dto/create-user.dto.ts
@@ -1,1 +1,17 @@
-export class CreateUserDto {}
+import { IsEmail, IsNotEmpty, IsString } from 'class-validator';
+
+export class CreateUserDto {
+  @IsEmail()
+  @IsNotEmpty()
+  email: string;
+  @IsString()
+  @IsNotEmpty()
+  password: string;
+
+  @IsString()
+  role: string;
+
+  firstName: string;
+  @IsString()
+  lastName: string;
+}
