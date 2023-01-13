cde4da121e8adb448ffbcebe1b6d66563a6c1822
Tee <teetat.wis@gmail.com>
2023-01-13 16:21:04 +0700

    feat: add course-search api

--- backend/src/course/course.service.ts
+++ backend/src/course/course.service.ts
@@ -52,7 +52,12 @@ export class CourseService {
     });
   }
 
-  async findAll(id: number) {
+  async findAll(
+    id: number,
+    name: string,
+    teacher: string,
+    description: string,
+  ) {
     if (id) {
       const result = await prisma.course.findMany({
         include: {
@@ -68,6 +73,13 @@ export class CourseService {
       });
     }
     return await prisma.course.findMany({
+      where: {
+        title: { contains: name },
+        instructor: {
+          name: { contains: teacher },
+        },
+        description: { contains: description },
+      },
       include: {
         instructor: true,
       },
