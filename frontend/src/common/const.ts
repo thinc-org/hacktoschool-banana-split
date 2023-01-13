1150d377461c8c9c2d225e545df2eac32a3defc1
MaStEr <masternonnolnw@gmail.com>
2023-01-13 10:45:29 +0700

    feat: init chat feature

--- frontend/src/common/const.ts
+++ frontend/src/common/const.ts
@@ -1,3 +1,4 @@
 export const socketURL = "https://banana-conference-server.songholder.co";
 export const baseApiURL = "https://banana-backend.songholder.co";
+export const chatSocketURL = "localhost:9000";
 // export const socketURL = "localhost:8000/";
