64b1829ddbdf3962395d0a13ecb7a772032bb303
Tee <teetat.wis@gmail.com>
2023-01-13 14:49:48 +0700

    fix: createCourse permission

--- backend/src/course/course.controller.ts
+++ backend/src/course/course.controller.ts
@@ -26,7 +26,7 @@ export class CourseController {
   create(@Request() req: any, @Body() createCourseDto: CreateCourseDto) {
     // admin
     const user = req.user;
-    if (user.role != Role.ADMIN) {
+    if (user.role == Role.STUDENT) {
       throw new UnauthorizedException();
     }
     return this.courseService.create(createCourseDto);
