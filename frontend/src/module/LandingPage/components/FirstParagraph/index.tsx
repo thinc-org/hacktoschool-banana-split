cf021f7564a9dc6d1ad90ae8417fa819f3b69be1
MaStEr <masternonnolnw@gmail.com>
2023-01-13 21:50:27 +0700

    style: more space in user count landingpage

--- frontend/src/module/LandingPage/components/FirstParagraph/index.tsx
+++ frontend/src/module/LandingPage/components/FirstParagraph/index.tsx
@@ -111,7 +111,8 @@ export default function FirstParagraph() {
             <div
               style={{
                 display: "flex",
-                flexDirection: "row"
+                flexDirection: "row",
+                marginBottom: "2px"
               }}
             >
               <BsLightningFill
