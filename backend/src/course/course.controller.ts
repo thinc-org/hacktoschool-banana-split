b348cd569f6b873f0e472cd7f0323be39b0ed5d4
Tee <teetat.wis@gmail.com>
2023-01-13 14:51:07 +0700

    fix: course permission

--- backend/src/course/course.controller.ts
+++ backend/src/course/course.controller.ts
@@ -24,7 +24,7 @@ export class CourseController {
   @UseGuards(AuthGuard('jwt'))
   @Post()
   create(@Request() req: any, @Body() createCourseDto: CreateCourseDto) {
-    // admin
+    // admin & teacher
     const user = req.user;
     if (user.role == Role.STUDENT) {
       throw new UnauthorizedException();
@@ -57,9 +57,9 @@ export class CourseController {
     @Param('id') id: string,
     @Body() updateCourseDto: UpdateCourseDto,
   ) {
-    // admin only
+    // admin & teacher
     const user = req.user;
-    if (user.role != Role.ADMIN) {
+    if (user.role == Role.STUDENT) {
       throw new UnauthorizedException();
     }
 
@@ -69,9 +69,9 @@ export class CourseController {
   @UseGuards(AuthGuard('jwt'))
   @Delete(':id')
   remove(@Request() req: any, @Param('id') id: string) {
-    // admin only
+    // admin & teacher
     const user = req.user;
-    if (user.role != Role.ADMIN) {
+    if (user.role == Role.STUDENT) {
       throw new UnauthorizedException();
     }
 
