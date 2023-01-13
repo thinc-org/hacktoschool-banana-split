45f2360a0804a5ca5ea7caf936b9a963b388be33
MaStEr <masternonnolnw@gmail.com>
2023-01-13 22:27:58 +0700

    style: make cleverse spinner in center

--- frontend/src/module/Loading/index.tsx
+++ frontend/src/module/Loading/index.tsx
@@ -9,10 +9,14 @@ export default function Loading() {
         height: "300px",
         background:
           "url(https://cdn-images-1.medium.com/max/1200/1*jzDNP1HfkAyPjofT3BWNxA.png)",
-        backgroundSize: "cover"
+        backgroundSize: "cover",
+        position: "fixed",
+        top: "calc(50% - 150px)",
+        left: "calc(50% - 150px)"
       }}
       animate={{
-        scale: [1, 1.2, 1.2, 1, 1]
+        scale: [1, 1.2, 1.2, 1, 1],
+        rotate: [0, 0, 360, 360, 0]
         // borderRadius: ["20%", "20%", "50%", "50%", "20%"]
       }}
       transition={{ repeat: Infinity, repeatDelay: 0.5, duration: 1.5 }}
